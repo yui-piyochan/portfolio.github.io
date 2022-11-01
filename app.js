12057d139011b189f0a0d6dd481865dcc4671129
kuwa-yui <72981378+yui-piyochan@users.noreply.github.com>
2022-11-01 19:15:33 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const switcher = document.querySelector('.btn');
+document.body.classList.toggle('light-theme');
+
+switcher.addEventListener('click', function() {
+    const className = document.body.className;
+
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+        document.body.classList.toggle('dark-theme');
+        document.body.classList.toggle('light-theme');
+    } else {
+        this.textContent = "Light";
+        document.body.classList.toggle('light-theme');
+        document.body.classList.toggle('dark-theme');
+    }
+
+    console.log('current class name: ' + className);
+});
